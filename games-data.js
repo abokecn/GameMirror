a6a40a719641c5ba5a11a4d7bb27b8dc08a4160e
慈父斯大林 <abokecn@hotmail.com>
2026-03-11 01:27:57 +0800

    Add games-data.js with initial game information

---
+++ games-data.js
@@ -0,0 +1,13 @@
+window.GAMES_WEB = {
+  games: [
+    {
+      name: "KARDS",
+      subdomain: "kards",
+      host: "kards.gamemirror.org",
+      description: "Official site mirror and resources",
+      status: "online",
+      visible: true,
+      sort: 10
+    }
+  ]
+};
